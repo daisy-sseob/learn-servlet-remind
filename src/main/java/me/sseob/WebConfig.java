d5bc716f948d2a8031ce81bbde89a881ea20f779
hyun-sseob <orolsyeo@gmail.com>
2021-09-13 20:47:42 +0900

    -📌 servlet remind, servlet에 spring IOC연동, spring framework configuration remind하기.

--- src/main/java/me/sseob/WebConfig.java
+++ src/main/java/me/sseob/WebConfig.java
@@ -3,8 +3,11 @@ package me.sseob;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -21,6 +24,8 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 @EnableWebMvc 
 public class WebConfig implements WebMvcConfigurer {
-
-	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/", ".jsp"); 
+	}
 }
