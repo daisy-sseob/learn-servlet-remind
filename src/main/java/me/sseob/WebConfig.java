52a7d9dab7db78f9beee3eebc3f026f5cee75f98
hyun-sseob <orolsyeo@gmail.com>
2021-10-09 16:52:03 +0900

    test commit

--- src/main/java/me/sseob/WebConfig.java
+++ src/main/java/me/sseob/WebConfig.java
@@ -1,15 +1,10 @@
 package me.sseob;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /*
 	EnableWebMvc을 사용하면 DelegatingWebMvcConfiguration를 import를 하기때문에
